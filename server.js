b52d0b04b178ea8c9762b8cda68fddc3df60407f
Philipp Doll <philipp.doll@9elments.com>
2020-07-05 09:26:58 +0200

    first setup of engine

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(3000, () =>
+  console.log('Example app listening on port 3000!'),
+);
